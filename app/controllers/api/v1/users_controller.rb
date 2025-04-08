97d3616c5803adde897e490188cedb064fd0ff3c
subhranshu <subhranshu.mfs@gmail.com>
2025-04-08 13:19:34 +0530

    2: Added Routes and controller for the User model

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,28 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+
+      def index
+        @users = User.all
+        render json:{
+          users_table: @users
+        }
+      end
+
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          render json: { message: 'User created successfully'}
+        else
+          render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      get '/users', to: 'users#index'
+      post '/signup', to: 'users#create'
+    end
+  end
+
 end
