efff272927d77aba263309060995e96ab10a1ee7
subhranshu <subhranshu.mfs@gmail.com>
2025-04-09 20:47:45 +0530

    3: Added Helper Methods

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -7,7 +7,8 @@ module Authentication
     AllowedList.create!(
       token: token,
       salt: salt,
-      expires_at: 24.hours.from_now
+      expires_at: 24.hours.from_now,
+      user: user
     )
     return token
   end
@@ -26,4 +27,29 @@ module Authentication
   def extract_token_from_header()
     request.headers['Authorization'].to_s.split(' ').last
   end
+
+  def current_user
+    token = extract_token_from_header
+    payload = JsonWebToken.decode(token) rescue nil
+
+    if payload.present?
+      salt = payload['salt']
+      allowed = AllowedList.find_by(token: token, salt: salt)
+      if allowed
+        return allowed.user
+      else
+        return nil
+      end
+    else
+      return nil
+    end
+  end
+
+  def is_authenticated?
+    if current_user
+      return true
+    else
+      return false
+  end
+  
 end
